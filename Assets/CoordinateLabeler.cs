cb33e312bb26712e593adb8389a24ade9caf9326
David Cresswell <djlcresswell@gmail.com>
2022-02-01 17:37:49 +0000

    code runs in edit mode and updates labels and object names

--- Assets/CoordinateLabeler.cs
+++ Assets/CoordinateLabeler.cs
@@ -2,16 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System;
 
 [ExecuteAlways] // edit mode and play mode - be carefull!!!
 public class CoordinateLabeler : MonoBehaviour
 {
-    // Update is called once per frame
+    TextMeshPro label;
+    Vector2Int coordinates = new Vector2Int();
+
+    void Awake() 
+    {
+        label = GetComponent<TextMeshPro>();
+        DisplayCoordinates();
+    }
     void Update()
     {
        if(!Application.isPlaying) //only run in editor mode
        {
-           //do something
+           DisplayCoordinates();
+           UpdateObjectName();
        } 
+    }
+    void DisplayCoordinates()
+    {
+        coordinates.x = Mathf.RoundToInt(transform.parent.position.x / UnityEditor.EditorSnapSettings.move.x);
+        coordinates.y = Mathf.RoundToInt(transform.parent.position.z/ UnityEditor.EditorSnapSettings.move.z);
+        label.text = $"{coordinates.x},{coordinates.y}";
+    }
+
+    private void UpdateObjectName()
+    {
+        transform.parent.name = coordinates.ToString();
     }
 }
