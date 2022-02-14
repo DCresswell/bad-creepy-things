70296d7a31cc9d8c47103ff737f229df67cf04d6
David Cresswell <djlcresswell@gmail.com>
2022-02-14 10:49:42 +0000

    first coroutine

--- Assets/Enemy/EnemyMover.cs
+++ Assets/Enemy/EnemyMover.cs
@@ -5,17 +5,19 @@ using UnityEngine;
 public class EnemyMover : MonoBehaviour
 {
     [SerializeField] List<Waypoint> path = new List<Waypoint>();
-
+    [SerializeField] float waitTime = 1f;
     void Start()
     {
-      PrintWaypointName();  
+     StartCoroutine(FollowPath());  
     }
 
-    void PrintWaypointName()
+    IEnumerator FollowPath() // coroutine
     {
     foreach(Waypoint waypoint in path)
         {
-            Debug.Log(waypoint.name);
+            //Debug.Log(waypoint.name);
+            this.transform.position = waypoint.transform.position;
+            yield return new WaitForSeconds(waitTime); //yield means give up control (here for 1 second)        
         }
     }
 
